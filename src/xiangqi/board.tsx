1daacfd71873ff3ea1b8654d70282221917f97d3
TungDT <tung.daothanhtung@gmail.com>
2020-05-12 05:40:41 +0700

    draggable piece

--- src/xiangqi/board.tsx
+++ src/xiangqi/board.tsx
@@ -8,7 +8,10 @@ import * as xq from './xiangqi_rule'
 
 
 export class Board extends React.Component {
-    state: BoardState;
+    state: {
+        // map square index to piece code
+        SparsePieces: Record<number, number>
+    };
 
     constructor(props: any) {
         super(props);
@@ -18,8 +21,10 @@ export class Board extends React.Component {
 
     makeMove(originSquare: number, targetSquare: number) {
         let movingPiece = this.state.SparsePieces[originSquare];
-        this.state.SparsePieces[targetSquare] = movingPiece;
-        this.state.SparsePieces[originSquare] = xq.EMPTY;
+        let newState = this.state;
+        newState.SparsePieces[targetSquare] = movingPiece;
+        newState.SparsePieces[originSquare] = xq.EMPTY;
+        this.setState(newState);
     }
 
     render() {
@@ -33,7 +38,7 @@ export class Board extends React.Component {
                         index: sqIdx,
                         visibility: xq.OffBoards[sqIdx],
                         piece: this.state.SparsePieces[sqIdx],
-                        MakeMove: this.makeMove,
+                        makeMove: this.makeMove,
                     }}
                     />
                 )}
@@ -42,61 +47,59 @@ export class Board extends React.Component {
     }
 }
 
-interface BoardState {
-    // map square index to piece code
-    SparsePieces: Record<number, number>
-}
-
-export function Square(props: SquareProps) {
+export function Square(props: {
+    index: number
+    visibility: csstype.VisibilityProperty
+    piece: number
+    // function to update the board state
+    makeMove: (originSquare: number, targetSquare: number) => void
+}) {
     const [{isOver}, drop] = reactDND.useDrop({
         accept: DraggableTypes.PIECE,
-        drop: () => props.MakeMove(0, props.index),
+        drop: (item, monitor) => {
+            let draggedProps: PieceProps = monitor.getItem().props;
+            props.makeMove(draggedProps.sqIdx, props.index);
+        },
         collect: monitor => ({
             isOver: !!monitor.isOver(),
         }),
     });
-    return (<button style={{
-        visibility: props.visibility,
-        width: `${100 / xq.BoardWidth}%`,
-        height: `${100 / xq.BoardHeight}%`,
-        fontSize: 8, color: "teal",
-    }}>
-        {props.index}<Piece piece={props.piece}/>
-    </button>)
-}
-
-interface SquareProps {
-    index: number
-    visibility: csstype.VisibilityProperty
-    piece: number
-    MakeMove: (originSquare: number, targetSquare: number) => void
+    return (
+        <button ref={drop}
+                style={{
+                    visibility: props.visibility,
+                    width: `${100 / xq.BoardWidth}%`,
+                    height: `${100 / xq.BoardHeight}%`,
+                    fontSize: 8, color: "teal",
+                }}>
+            {props.index}<Piece {...{piece: props.piece, sqIdx: props.index}}/>
+        </button>)
 }
 
 export function Piece(props: PieceProps) {
     let [{isDragging}, drag] = reactDND.useDrag({
-        item: {type: DraggableTypes.PIECE},
+        item: {type: DraggableTypes.PIECE, props: props},
         collect: monitor => ({
             isDragging: !!monitor.isDragging(),
         }),
     });
     let pieceSymbol = xq.PieceSymbols.get(props.piece);
     return (
-        <span
-            ref={drag}
-            style={{
-                opacity: isDragging ? 0.5 : 1, cursor: 'move',
-                color: "black", zIndex: 1, fontSize: 25,
-            }}
-        >
+        <span ref={drag}
+              style={{
+                  opacity: isDragging ? 0.5 : 1, cursor: 'move',
+                  color: "black", zIndex: 1, fontSize: 25,
+              }}>
             {pieceSymbol}
         </span>
     )
 }
 
 interface PieceProps {
-    piece: number
+    piece: number,
+    // current square this piece belongs to
+    sqIdx: number
 }
-
 
 export const DraggableTypes = {
     PIECE: 'PIECE',
